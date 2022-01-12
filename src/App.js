17e5e85e5e568509122a842811f7f69e88fe0095
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-01-13 04:46:51 +0900

    [start] grama-front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [],
+    }
+    this.host = "http://localhost:3001/"
+  }
+
+  componentDidMount() {
+    axios
+      .get(`${this.host}todos`)
+      .then(res => {
+        this.setState({ todos: res.data })
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+
+  deleteTodo = id => {
+    const { todos } = this.state
+
+    axios
+      .delete(`${this.host}todos/${id}`)
+      .then(res => {
+        this.setState({
+          todos: todos.filter(todo => {
+            return todo.id !== id
+          }),
+        })
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
+
+  addTodo = todo => {
+    const { todos } = this.state
+    axios
+      .post(`${this.host}todos`, todo)
+      .then(res => {
+        this.setState({
+          todos: [...todos, res.data],
+        })
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
 }
 
-export default App;
+export default App
